09335927f4e3bf4fef91f25a46bfcaa61f9a5757
carololiveirapaes1 <carol.paes@aluno.ifsp.edu.br>
2024-07-30 11:31:22 -0300

    hgbhlgb

---
+++ criarbanco.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS IFSP;
+CREATE DATABASE IF NOT EXISTS IFSP;
+USE IFSP;
+
+CREATE TABLE Cidade
+(
+    id     INT AUTO_INCREMENT,
+    nome   VARCHAR(100),
+    estado VARCHAR(2),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Pessoa
+(
+    id        INT AUTO_INCREMENT,
+    nome      VARCHAR(100),
+    email     VARCHAR(100),
+    endereco  VARCHAR(50),
+    bairro    VARCHAR(50),
+    cep       VARCHAR(50),
+    ativo     BOOL,
+    id_cidade INT,
+    PRIMARY KEY (id),
+    CONSTRAINT FK_PessoaCidade FOREIGN KEY (id_cidade) REFERENCES Cidade(id)
+);
+
+CREATE TABLE Animal
+(
+    id             INT AUTO_INCREMENT,
+    nome           VARCHAR(100),
+    especie        VARCHAR(100),
+    raca           VARCHAR(50),
+    data_nascimento DATE,
+    idade          VARCHAR(50),
+    cadastro       VARCHAR(50),
+    ativo          BOOL,
+    id_pessoa      INT,
+    PRIMARY KEY (id),
+    CONSTRAINT FK_AnimalPessoa FOREIGN KEY (id_pessoa) REFERENCES Pessoa(id)
+);
